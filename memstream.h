d62299c014089240cc31a92d56b310f63c97dece
Kegan Hollern <keganhollern@gmail.com>
2023-03-08 10:57:47 -0600

    started improving header documentation

--- memstream.h
+++ memstream.h
@@ -4,14 +4,16 @@
 #include <vmmdll.h>
 #include <stdint.h>
 
-//--- STRUCTURES
+//========== STRUCTURES ===========
 
-// MSSContext is a global context for DMA interaction.
+// MSSContext is a global context for DMA
+// interaction.
 typedef struct MSSContext {
     VMM_HANDLE hVMM;
 } MSSContext, *PMSSContext;
 
-// MSSProcess represents a process on the remote machine.
+// MSSProcess represents a process on the
+// remote machine.
 typedef struct MSSProcess {
     PMSSContext ctx;
     uint64_t pid;
@@ -44,52 +46,155 @@ typedef struct ModuleList {
     const char* name;
 } ModuleList, *PModuleList;
 
-
-//--- PCIE FPGA
-
-HRESULT MSS_InitFPGA(PMSSContext* pCtx);
-HRESULT MSS_CloseContext(PMSSContext ctx);
-HRESULT MSS_DisableMasterAbort(PMSSContext ctx);
-
-//--- UTILS
-
-void MSS_PrintBuffer(void* buffer, size_t size);
-
-//--- PROCESS
-
-HRESULT MSS_GetProcess(PMSSContext ctx, const char* name, PMSSProcess* pProcess);
-HRESULT MSS_GetAllProcesses(PMSSContext ctx, const char* name, PProcessList* pProcessList);
-
-HRESULT MSS_GetModuleBase(PMSSProcess process, const char* name, uint64_t* pBase);
-HRESULT MSS_GetModuleExports(PMSSProcess process, const char* name, PExportList* pExportList);
-HRESULT MSS_GetModuleExport(PMSSProcess process, const char* module, const char* export, uint64_t* pAddress);
-HRESULT MSS_GetModuleImports(PMSSProcess process, const char* name, PImportList* pImportList);
-HRESULT MSS_GetModuleImport(PMSSProcess process, const char* module, const char* export, uint64_t* pAddress);
-HRESULT MSS_GetProcessModules(PMSSProcess process, PModuleList* pModuleList);
-
-
-//--- READ
-
-HRESULT MSS_ReadSingle(PMSSProcess process, uint64_t address, void* buffer, size_t size);
-
+// MSSReadArray is an array of addresses,
+// buffers, and buffer sizes. This is used
+// for performant reads.
+// interchangeable with MSSWriteArray.
 typedef struct MSSReadArray {
-    uint64_t* read_addresses;
-    void** read_buffers;
-    size_t* read_sizes;
+    uint64_t* addresses;
+    void** buffers;
+    size_t* sizes;
     size_t count;
     size_t capacity;
 } MSSReadArray, *PMSSReadArray;
+// MSSWriteArray is an array of addresses,
+// buffers, and buffer sizes. This is used
+// for performant writes.
+// interchangeable with MSSReadArray
+typedef MSSReadArray MSSWriteArray;
+// todo: probably better to rename readarray and have it handle both ops
+typedef PMSSReadArray PMSSWriteArray;
 
-HRESULT MSS_ReadMany(PMSSProcess process, uint64_t addresses[], void* buffers[], size_t sizes[], size_t count);
+//========== PCIE FPGA ===========
 
-HRESULT MSS_PushRead(PMSSReadArray array, uint64_t address, void* buffer, size_t size); // add element to read array
-HRESULT MSS_PushManyReads(PMSSReadArray array, uint64_t addresses[], void* buffers[], size_t sizes[], size_t count);
-HRESULT MSS_FreeRead(PMSSReadArray array); // delete read array
-HRESULT MSS_NewReadArray(size_t capacity, PMSSReadArray* pArray); // construct read array
+// MSS_InitFPGA initializes the DMA device and
+// returns an MSSContext.
+HRESULT MSS_InitFPGA(PMSSContext* pCtx);
 
-//--- WRITE
+// MSS_CloseContext closes the MSSContext and
+// releases the underlying DMA device handles.
+HRESULT MSS_CloseContext(PMSSContext ctx);
 
-HRESULT MSS_WriteSingle(PMSSProcess process, uint64_t addres, void* buffer, size_t size);
+// MSS_DisableMasterAbort sets auto-clear for
+// the master abort flag on the DMA device.
+HRESULT MSS_DisableMasterAbort(PMSSContext ctx);
+
+//========= UTILS =========
+
+// MSS_PrintBuffer is a utility for printing a
+// large byte buffer in a pretty way.
+void MSS_PrintBuffer(void* buffer, size_t size);
+
+//=========== PROCESS ==========
+
+// MSS_GetProcess acquires the remote process
+// and returns an MSSProcess.
+HRESULT MSS_GetProcess(
+        PMSSContext ctx,
+        const char* name,
+        PMSSProcess* pProcess);
+
+// MSS_GetAllProcesses returns a list of all
+// remote processes matching the provided name.
+HRESULT MSS_GetAllProcesses(
+        PMSSContext ctx,
+        const char* name,
+        PProcessList* pProcessList);
+
+// MSS_GetModuleBase returns the base address
+// of the module within the remote process.
+HRESULT MSS_GetModuleBase(
+        PMSSProcess process,
+        const char* name,
+        uint64_t* pBase);
+
+// MSS_GetModuleExports returns a list of all
+// exported symbols from the provided module.
+HRESULT MSS_GetModuleExports(
+        PMSSProcess process,
+        const char* name,
+        PExportList* pExportList);
+
+// MSS_GetModuleExport returns the address of
+// the provided exported symbol.
+HRESULT MSS_GetModuleExport(
+        PMSSProcess process,
+        const char* module,
+        const char* export,
+        uint64_t* pAddress);
+
+// MSS_GetModuleImports returns a list of all
+// imported symbols in the provided module.
+HRESULT MSS_GetModuleImports(
+        PMSSProcess process,
+        const char* name,
+        PImportList* pImportList);
+
+// MSS_GetModuleImport returns the address of
+// the imported symbol in the provided module.
+HRESULT MSS_GetModuleImport(
+        PMSSProcess process,
+        const char* module,
+        const char* export,
+        uint64_t* pAddress);
+
+// MSS_GetProcessModules returns a list of all
+// module names in the process.
+HRESULT MSS_GetProcessModules(
+        PMSSProcess process,
+        PModuleList* pModuleList);
+
+//========= READ ==========
+
+// MSS_ReadSingle reads a single address.
+// This is unoptimized and best used sparingly.
+HRESULT MSS_ReadSingle(
+        PMSSProcess process,
+        uint64_t address,
+        void* buffer,
+        size_t size);
+
+// MSS_ReadMany reads many addresses.
+// This is optimized. It is better to call
+// MSS_ReadMany once with many addresses than
+// to call MSS_ReadSingle many times.
+HRESULT MSS_ReadMany(
+        PMSSProcess process,
+        uint64_t addresses[],
+        void* buffers[],
+        size_t sizes[],
+        size_t count);
+// MSS_PushRead pushes a read operation to
+// an MSSReadArray. MSSReadArray must have
+// capacity.
+HRESULT MSS_PushRead(
+        PMSSReadArray array,
+        uint64_t address,
+        void* buffer,
+        size_t size);
+
+// MSS_PushManyReads pushes many read
+// operations to an MSSReadArray.
+// MSSReadArray must have capacity.
+HRESULT MSS_PushManyReads(
+        PMSSReadArray array,
+        uint64_t addresses[],
+        void* buffers[],
+        size_t sizes[],
+        size_t count);
+
+// MSS_FreeRead releases an MSSReadArray.
+HRESULT MSS_FreeRead(PMSSReadArray array);
+
+// MSS_NewReadArray constructs an
+// MSSReadArray with a fixed capacity.
+HRESULT MSS_NewReadArray(
+        size_t capacity,
+        PMSSReadArray* pArray);
+
+//========== WRITE ==========
+
+HRESULT MSS_WriteSingle(PMSSProcess process, uint64_t address, void* buffer, size_t size);
 HRESULT MSS_WriteMany(PMSSProcess process, uint64_t addresses[], void* buffers[], size_t sizes[], size_t count);
 
 //--- MEMORY ROUTINES
